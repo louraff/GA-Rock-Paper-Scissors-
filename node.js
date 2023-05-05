c796540e8d3b6f9a572ed95716c0604fe45b9143
Louise Raffray <raffraylouise@gmail.com>
2023-05-05 10:48:33 +0100

    added renderScores()

---
+++ node.js
@@ -0,0 +1,50 @@
+/*----- constants -----*/
+const AUDIO = new Audio('https://assets.mixkit.co/sfx/preview/mixkit-simple-countdown-922.mp3');
+
+/*----- app's state (variables) -----*/
+let scores; // Object key of 'p' -> player score
+            // key of 't' -> tie; key of 'c' -> computer score
+let results; // Object key of 'p' for the player result
+            // values 'r' rock, 'p' -> paper, 's' -> scissors;
+let winner; // String 'p' if the player wins, 't' for tie, 'c' if computer wins
+
+/*----- cached element references -----*/
+
+
+/*----- event listeners -----*/
+
+
+/*----- functions -----*/
+init()
+//INITIALISE ALL STATE THEN CALL RENDER
+function init() {
+  scores = {
+    p: 0, 
+    t: 0,
+    c: 0
+  };
+
+  results = {
+    p: 'r',
+    c: 'r'
+  };
+
+  winner = 't';
+
+function renderScores() {
+  for (let key in scores) { // for in loop to iterate over keys in an object
+    const scoreEl = document.getElementById(`${key}-score`)
+    scoreEl.innerText = scores[key];
+  }
+};
+
+function renderResults() {
+  
+};
+
+  function render() {
+    renderScores();
+    renderResults();
+  }
+};
+
